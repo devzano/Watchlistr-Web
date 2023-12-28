7d9af1e269fbc12e9e6704e2307ee57feb01a319
Ruben Manzano <rubenizag@gmail.com>
2023-12-28 09:16:54 -0500

    Update PopularMedia.jsx

--- React/src/PopularMedia.jsx
+++ React/src/PopularMedia.jsx
@@ -41,7 +41,7 @@ const PopularMedia = () => {
                     <p>Genres: N/A</p>
                   )}
                   <p>{movie.overview}</p>
-                  <Link to={`/watch/${movie.id}/embed`}>Watch Movie</Link>
+                  <Link to={`/watch/${movie.tmdbId}/embed`}>Watch Movie</Link>
                   <Link to={`/movies/${movie.id}`}>View Trailers</Link>
                   <br/>
                   <Link to={`/movies/${movie.id}/reviews`}>Reviews</Link>

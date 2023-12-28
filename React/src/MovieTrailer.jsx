7819ecd567dc96ce73149548d3c89cd771f45e5a
Ruben <rubenizag@aol.com>
2023-12-28 17:17:18 -0500

    updates

--- React/src/MovieTrailer.jsx
+++ React/src/MovieTrailer.jsx
@@ -5,9 +5,10 @@ import { useParams } from 'react-router-dom';
 const apiKey = process.env.REACT_APP_TMDB_API_KEY;
 
 function MovieTrailers() {
-  const { id } = useParams();
+  const {id} = useParams();
   const [movieTitle, setMovieTitle] = useState('');
-  const [movie, setMovie] = useState(null);
+  const [trailers, setTrailers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
@@ -20,37 +21,58 @@ function MovieTrailers() {
       }
     };
 
-    const fetchMovie = async () => {
-      const url = `https://vidsrc.xyz/embed/movie?tmdb=${id}`;
+    const fetchTrailers = async (page) => {
+      const url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${apiKey}&page=${page}`;
       try {
         const { data } = await axios.get(url);
-        setMovie(data.results);
+        setTrailers(data.results);
       } catch (error) {
         console.error(error);
       }
     };
 
     fetchMovieDetails();
-    fetchMovie();
-  }, [id]);
+    fetchTrailers(currentPage);
+  }, [id, currentPage]);
+
+  const totalPages = Math.ceil(trailers.length / 10);
+
+  const handlePageClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const renderTrailers = () => {
+    const startIndex = (currentPage - 1) * 10;
+    const endIndex = startIndex + 10;
+
+    return trailers.slice(startIndex, endIndex).map((trailer) => trailer.key && (
+      <div key={trailer.id}>
+        <h3>{trailer.name}</h3>
+        <iframe
+          style={{border: "none"}}
+          width="560"
+          height="315"
+          src={`https://www.youtube.com/embed/${trailer.key}`}
+          title={trailer.name}
+          allowFullScreen
+        ></iframe>
+      </div>
+    ));
+  };
 
   return (
     <div className="nav-container">
-      {movieTitle ? <h1>{movieTitle} movie</h1> : <h1>Loading...</h1>}
-      {movie && (
-        <div key={movie.id}>
-          <h3>{movie.name}</h3>
-          <iframe
-            style={{ border: "none" }}
-            width="560"
-            height="315"
-            src={`https://vidsrc.xyz/embed/movie?tmdb=${movie.id}`}
-            title={movie.name}
-            allowFullScreen
-          ></iframe>
-        </div>
-      )}
+      {movieTitle ? <h1>{movieTitle} Trailer(s)</h1> : <h1>Loading...</h1>}
+      {renderTrailers()}
+      <div>
+        {Array.from({ length: totalPages }).map((_, index) => (
+          <button key={index} onClick={() => handlePageClick(index + 1)}>
+            {index + 1}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
+
 export default MovieTrailers;

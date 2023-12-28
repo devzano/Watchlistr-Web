1c8f7f852a443a2005d813b5e1205a7ba006ffa5
Ruben <rubenizag@aol.com>
2023-12-28 17:38:58 -0500

    updates

--- React/src/MovieTrailer.jsx
+++ React/src/MovieTrailer.jsx
@@ -5,7 +5,7 @@ import { useParams } from 'react-router-dom';
 const apiKey = process.env.REACT_APP_TMDB_API_KEY;
 
 function MovieTrailers() {
-  const {id} = useParams();
+  const { id } = useParams();
   const [movieTitle, setMovieTitle] = useState('');
   const [trailers, setTrailers] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
@@ -49,11 +49,20 @@ function MovieTrailers() {
       <div key={trailer.id}>
         <h3>{trailer.name}</h3>
         <iframe
-          style={{border: "none"}}
+          style={{ border: "none" }}
           width="560"
           height="315"
           src={`https://www.youtube.com/embed/${trailer.key}`}
           title={trailer.name}
+          allowFullScreen
+        ></iframe>
+        {/* Add the embedded video URL for each trailer */}
+        <iframe
+          style={{ border: "none" }}
+          width="560"
+          height="315"
+          src={`https://vidsrc.xyz/embed/movie?tmdb=${id}&sub_url=https%3A%2F%2Fvidsrc.me%2Fsample.srt`}
+          title={`Embedded ${trailer.name}`}
           allowFullScreen
         ></iframe>
       </div>

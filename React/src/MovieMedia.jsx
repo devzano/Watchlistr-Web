69da112115771d1442142158dabd90aceab3ad5d
Ruben <rubenizag@aol.com>
2024-01-01 14:10:10 -0500

    updates

--- React/src/MovieMedia.jsx
+++ React/src/MovieMedia.jsx
@@ -8,8 +8,6 @@ function MovieMedia() {
   const isMobile = window.innerWidth <= 768;
   const {id} = useParams();
   const [movieTitle, setMovieTitle] = useState('');
-  const [trailers, setTrailers] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
@@ -22,44 +20,8 @@ function MovieMedia() {
       }
     };
 
-    const fetchTrailers = async (page) => {
-      const url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${apiKey}&page=${page}`;
-      try {
-        const { data } = await axios.get(url);
-        setTrailers(data.results);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
     fetchMovieDetails();
-    fetchTrailers(currentPage);
-  }, [id, currentPage]);
-
-  const totalPages = Math.ceil(trailers.length / 10);
-
-  const handlePageClick = (pageNumber) => {
-    setCurrentPage(pageNumber);
-  };
-
-  const renderTrailers = () => {
-    const startIndex = (currentPage - 1) * 10;
-    const endIndex = startIndex + 10;
-
-    return trailers.slice(startIndex, endIndex).map((trailer) => trailer.key && (
-      <div key={trailer.id}>
-        <h3>{trailer.name}</h3>
-        <iframe
-          style={{ border: "none" }}
-          width={isMobile ? '100%' : '560px'}
-          height="315"
-          src={`https://www.youtube.com/embed/${trailer.key}`}
-          title={trailer.name}
-          allowFullScreen
-        ></iframe>
-      </div>
-    ));
-  };
+  }, [id]);
 
   const movieURL = `https://vidsrc.xyz/embed/movie?tmdb=${id}`;
 
@@ -77,16 +39,6 @@ function MovieMedia() {
           title={movieTitle}
           allowFullScreen
         ></iframe>
-      </div>
-
-      {renderTrailers()}
-      <div>
-      <h3>{movieTitle} Trailer(s)</h3>
-        {Array.from({ length: totalPages }).map((_, index) => (
-          <button key={index} onClick={() => handlePageClick(index + 1)}>
-            {index + 1}
-          </button>
-        ))}
       </div>
     </div>
   );
